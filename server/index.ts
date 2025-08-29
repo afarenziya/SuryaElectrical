102c60e0e373fec342e2c63dd7bb4c77c20adc4a
ariynary <your-email@example.com>
2025-08-30 00:06:24 +0530

    Initial commit for Surya Electrical website

--- server/index.ts
+++ server/index.ts
@@ -3,6 +3,7 @@ import { registerRoutes } from "./routes";
 import { setupVite, serveStatic, log } from "./vite";
 
 const app = express();
+const PORT = process.env.PORT || 4000;
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
@@ -61,11 +62,7 @@ app.use((req, res, next) => {
   // this serves both the API and the client.
   // It is the only port that is not firewalled.
   const port = parseInt(process.env.PORT || '5000', 10);
-  server.listen({
-    port,
-    host: "0.0.0.0",
-    reusePort: true,
-  }, () => {
-    log(`serving on port ${port}`);
+  server.listen(port, 'localhost', () => {
+    log(`serving on http://localhost:${port}`);
   });
 })();
